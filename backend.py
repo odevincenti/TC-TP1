84d5b3a84b428ea519c016f40d79f8902dc6fc0d
odevincenti <odevincenti@outlook.com>
2021-08-07 06:36:06 -0300

    Agrego Curvespace y Teo, funcionan para 1 curva, más no llegué a testear

--- backend.py
+++ backend.py
@@ -3,42 +3,67 @@ import matplotlib.pyplot as plt
 import scipy.signal as ss
 
 ########################################################################################################################
-# Clase Curve: Contiene toda la información para graficar la curva. Necesita:
-#    - Tipo de curva: - 1 si es teórica (función transferencia)
-#                     - 2 si es simulada (LTSpice)
-#                     - 3 si es medida (Digilent)
-#                     - 0 si es otra cosa (error)
-#    - Datos: Dependerán del tipo de curva, en cada caso se especifica mejor (mirar funciones)
-#    - Nombre: Si no se especifica, se le asignará uno según el orden
-#    - Color: Se permitirá elegir el color de la curva, si no se especifica se tomará naranja todo ver como hace esto la gui
-# Para cada tipo se accede una función particular, estas devuelven 0 si se creó bien o 1 si hubo un error
+# Clase Curvespace: Contiene la lista de curvas y métodos para modificarla
 # ----------------------------------------------------------------------------------------------------------------------
+class Curvespace:
+    def __init__(self):
+        self.curves = []        # Arreglo de curvas
 
-class Curve:
-    def __init__(self, c_type, data, name="", color='orange'):
-        self.type = c_type          # Tipo de curva
-        self.data = data
-        self.color = color          # Color de la curva
-        if name != "": self.name = name
-        else: self.name = "curva1"
-        self.update()
+    # addCurve: Método para agregar una curva. Para más detalles mirar clase Curva
+    def addCurve(self, c_type, data, name="", color='orange'):
+        if name == "" or not self.checkName(name):
+            for i in range(len(self.curves) + 1):
+                name = "Curve " + str(len(self.curves) - i)
+                if self.checkName(name): break
 
-    def update(self):
-        r = self.switch_ctypes.get(self.type, self.c_type_error)(self)
+        if not self.switch_ctypes.get(c_type, self.c_type_error)(self, data, name, color):
+            print("Error creando la curva")
+
+    # checkName: Revisa si el nombre que se quiere asignar ya existe
+    # Devuelve False si ya existe, True si está disponible
+    def checkName(self, name):
+        r = True
+        for i in range(len(self.curves)):
+            if name == self.curves[i]:
+                r = False
+                print("El nombre que quiere asignar ya existe")
+                break
         return r
 
-    def data_check(self, data):
-        return 1
+    # changeCurveName: Setter para el nombre de una curva. Recibe:
+    #   - index: índice de la curva en el arreglo (lo puedo cambiar a nombre o a la curva en sí lo que les resulte más cómodo)
+    #   - name: nombre nuevo para la curva
+    # Devuelve False en caso de error (el nuevo nombre ya está asignado)
+    def changeCurveName(self, index, name):
+        r = self.checkName(name)
+        if r: self.curves[index].name = name
+        return r
 
-    def teorica(self):
+    # changeCurveColor: Setter para el color de una curva. Recibe:
+    #   - index: índice de la curva en el arreglo (lo puedo cambiar a nombre o a la curva en sí lo que les resulte más cómodo)
+    #   - color: color nuevo para la curva
+    # Devuelve False en caso de error
+    def changeCurveColor(self, index, color):
+        r = True
+        self.curves[index].color = color
+        return r
+
+    def teorica(self, data, name, color):
         print("teórica")
-        return 0
+        r = True
+        t = Teo(1, data, name, color)
+        if t.H is not None:
+            self.curves.append(t)
+        else:
+            print("Los datos ingresados no son válidos")
+            r = False
+        return r
 
-    def simulada(self):
+    def simulada(self, data, name, color):
         print("simulada")
         return 0
 
-    def medida(self):
+    def medida(self, data, name, color):
         print("medida")
         return 0
 
@@ -54,7 +79,116 @@ class Curve:
     }
 ########################################################################################################################
 
+########################################################################################################################
+# Clase Curve: Contiene toda la información para graficar la curva. Necesita:
+#    - Tipo de curva: - 1 si es teórica (función transferencia)
+#                     - 2 si es simulada (LTSpice)
+#                     - 3 si es medida (Digilent)
+#                     - 0 si es otra cosa (error)
+#    - Datos: Dependerán del tipo de curva, en cada caso se especifica mejor (mirar funciones)
+#    - Nombre: Si no se especifica, se le asignará uno según el orden
+#    - Color: Se permitirá elegir el color de la curva, si no se especifica se tomará naranja todo ver como hace esto la gui
+# Para cada tipo se accede una clase particular, mirarlas para más detalles
+# ----------------------------------------------------------------------------------------------------------------------
+class Curve:
+    def __init__(self, c_type, data, name, color):
+        self.type = c_type          # Tipo de curva
+        self.rawdata = data         # Datos como se cargan
+        self.name = name            # Nombre de la curva
+        self.color = color          # Color de la curva
+        self.visibility = True      # Está visible? Por defecto se inicializa en True
 
+    # check_data: Método para la verificación de datos (método virtual)
+    def check_data(self, data):
+        return
 
+    # change_data: Setter para la modificación de datos (método virtual)
+    def change_data(self, data):
+        return
+########################################################################################################################
+
+########################################################################################################################
+# Clase Teo: Curva teórica, hija de la clase Curve
+# Tiene algunos parámetros extra:
+#       - H: Función Transferencia (scipy)
+# ----------------------------------------------------------------------------------------------------------------------
+class Teo(Curve):
+    def __init__(self, c_type, data, name, color):
+        super().__init__(1, data, name, color)
+        num, den = self.check_data(self.rawdata)
+        self.H = None
+        if num is not None and den is not None:         # Si están en orden, hace la modificación
+            self.H = ss.TransferFunction(num, den)
+
+    # change_data: Revisa la validez de los datos nuevos. Devuelve False si hubo error.
+    def change_data(self, data):
+        r = True
+        num, den = self.check_data(self.rawdata)        # Revisa los datos nuevos
+        if num is not None and den is not None:         # Si están en orden, hace la modificación
+            self.H = ss.TransferFunction(num, den)
+        else:
+            print("Los datos ingresados no son válidos")
+            r = False
+        return r
+
+    # check_data: Procesa los datos de la función transferencia, asume que se recibe un arreglo con el numerador y denominador como strings
+    # Ej data = ["1, 2,1, ,5", "3, 4, 1,3"]
+    # Los espacios de más se eliminan y los números racionales se ingresan con punto (1.5)
+    # Devuelve num = None y den = None si hubo error
+    def check_data(self, data):
+        num = fix_coefs(data[0])
+        den = fix_coefs(data[1])
+        '''
+        num = data[0]
+        den = data[1]
+        num = num.replace(" ", "")      # Elimina espacios
+        den = den.replace(" ", "")
+        if num == "" or den == "":
+            r = False
+            print("Por favor ingrese los coeficientes")
+        num = num.replace(",,", ",0,")      # Pone ceros donde falta el coeficiente
+        den = den.replace(",,", ",0,")
+        if num[-1] == ",": num = num + "0"  # Considera coeficientes cero al final
+        if den[-1] == ",": den = den + "0"
+        num = num.split(",")
+        den = den.split(",")
+        try:
+            num = [float(s) for s in num]
+            den = [float(s) for s in den]
+        except ValueError:
+            print("Uno de los valores ingresados no es numérico")
+            r = False
+        if not r:
+            num = None
+            den = None
+        '''
+        return num, den
+########################################################################################################################
+
+########################################################################################################################
+# fix_coefs: Acomoda los coeficientes del numerador o denominador y revisa si están bien ingresados
+#            Devuelve None en caso de error
+# ----------------------------------------------------------------------------------------------------------------------
+def fix_coefs(coefs):
+    r = True
+    coefs = coefs.replace(" ", "")  # Elimina espacios
+    if coefs == "":
+        r = False
+        print("Por favor ingrese los coeficientes")
+    coefs = coefs.replace(",,", ",0,")
+    if coefs[0] == ",":
+        coefs = coefs[1:]
+    if coefs[-1] == ",":
+        coefs = coefs + "0"
+    coefs = coefs.split(",")
+    try:
+        coefs = [float(s) for s in coefs]
+    except ValueError:
+        print("Uno de los valores ingresados no es numérico")
+        r = False
+    if not r:
+        coefs = None
+    return coefs
+########################################################################################################################
 
 
--- test_backend.py
+++ test_backend.py
@@ -1,12 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as ss
-from backend import Curve
+from backend import Curvespace
 
 # ARCHIVO PARA PROBAR LAS FUNCIONALIDADES DEL BACKEND ANTES DE AGREGAR EL FRONT
 
-c1 = Curve(1, [1, 2, 3], color="blue")
-print(c1.type)
-print(c1.name)
-print(c1.color)
+def plot_bode_mod(c, ax):
+    w, mod, ph = ss.bode(c.H)#, np.linspace(F.w0*10**-2, F.w0*10**2, 10**4))       # Obtengo valores de frecuencia y módulo
+    ax.semilogx(w, mod, c.color)  # Grafico bode del módulo de la transferencia
+    plt.legend(c.name)
+    ax.set_xlabel("$\omega$ $[log(s)]$")
+    ax.set_ylabel("$|H(S)|$ $[dB]$")
+    ax.grid()
+    return
+
+def plot_bode_ph(c, ax):
+    w, mod, ph = ss.bode(c.H)#, np.linspace(F.w0*10**-2, F.w0*10**2, 10**4))       # Obtengo valores de frecuencia y fase
+    ax.semilogx(w, ph, c.color)   # Grafico bode de la fase de la transferencia
+    plt.legend(c.name)
+    ax.set_xlabel("$\omega$ $[log(s)]$")
+    ax.set_ylabel("$\phi(H(s))$ $[°]$")
+    ax.grid()
+    return
+
+# TEST
+C = Curvespace()
+C.addCurve(1, [",1, 2, 3,", "10  , 20 , 30"], name="Prueba teórica", color="blue")
+print(C.curves[0].name)
+print(C.curves[0].H)
+print(C.curves[0].color)
+
+fig, ax = plt.subplots(2, 1)
+fig.suptitle("Bode plot")
+plot_bode_mod(C.curves[0], ax[0])        # Grafico módulo
+plot_bode_ph(C.curves[0], ax[1])         # Grafico fase
+fig.tight_layout()
+plt.show()
+
+
+
+
 
