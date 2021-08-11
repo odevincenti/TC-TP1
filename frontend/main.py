d0f21a4f682284c31ffa79942698e50f41b57ac9
valen <vvenier@itba.edu.ar>
2021-08-11 18:32:59 -0300

    curvas ok todo piola

--- frontend/main.py
+++ frontend/main.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUi
 from backend import *
 from mplwidget import MplWidget
+from mplwidget import MplWidget2
 
 import numpy as np
 import random
@@ -24,7 +25,6 @@ class Input_Teorico_Window(QWidget):
         numerador_input = self.numerador_teorico.text()
         denominador_input = self.denominador_teorico.text()
         cs.add_curve(1, [numerador_input, denominador_input], nombre_input)
-        #self.show_graph()
         self.close()
 
 
@@ -37,9 +37,16 @@ class Input_Simulacion_Window(QWidget):
 
         self.show()
         self.upload_simulacion_pushButton.clicked.connect(self.get_simulation_file)
+        self.ok_simulacion_pushButton_2.clicked.connect(self.display_ok)
 
     def get_simulation_file(self):
-        QFileDialog.getOpenFileNames()
+        filename = QFileDialog.getOpenFileNames()
+        self.path = filename[0][0]
+
+
+    def display_ok(self):
+        cs.add_curve(2, self.path)
+        self.close()
 
 
 class Input_Medicion_Window(QWidget):
@@ -50,9 +57,15 @@ class Input_Medicion_Window(QWidget):
 
         self.show()
         self.upload_medicion_pushButton.clicked.connect(self.get_medicion_file)
+        self.ok_medicion_pushButton_2.clicked.connect(self.display_ok)
 
     def get_medicion_file(self):
-        QFileDialog.getOpenFileNames()
+        filename = QFileDialog.getOpenFileNames()
+        self.path = filename[0][0]
+
+    def display_ok(self):
+        cs.add_curve(3, self.path)
+        self.close()
 
 
 class MatplotlibWidget(QtWidgets.QMainWindow):
@@ -68,6 +81,7 @@ class MatplotlibWidget(QtWidgets.QMainWindow):
         self.teoricoButton.clicked.connect(self.goto_graphInfoTeorico)
         self.simulacionButton.clicked.connect(self.goto_graphInfoSimulacion)
         self.medicionButton.clicked.connect(self.goto_graphInfoMedicion)
+        self.borrarpushButton.clicked.connect(self.goto_borrar)
 
         self.moduloButton.clicked.connect(self.goto_graphModulo_Axis)
         self.faseButton.clicked.connect(self.goto_graphFase_Axis)
@@ -83,9 +97,11 @@ class MatplotlibWidget(QtWidgets.QMainWindow):
 
     def goto_graphInfoSimulacion(self):
         self.Input_Simulacion = Input_Simulacion_Window()
+        self.Input_Simulacion.ok_simulacion_pushButton_2.clicked.connect(self.show_graph)
 
     def goto_graphInfoMedicion(self):
         self.Input_Medicion = Input_Medicion_Window()
+        self.Input_Medicion.ok_medicion_pushButton_2.clicked.connect(self.show_graph)
 
     def goto_graphModulo_Axis(self):
         if len(self.ejextextEdit.text()) == 0:
@@ -112,14 +128,25 @@ class MatplotlibWidget(QtWidgets.QMainWindow):
 
     def show_graph(self):
         self.MplWidget.canvas.axes.clear()
+        self.MplWidget2.canvas.axes.clear()
         print(len(cs.curves))
         if len(cs.curves) != 0:
             cs.plot_mod(self.MplWidget.canvas.axes)
+            cs.plot_ph(self.MplWidget2.canvas.axes)
             self.MplWidget.canvas.draw()
+            self.MplWidget2.canvas.draw()
 
     def hide_graph(self):
         self.MplWidget.canvas.axes.clear()
         self.MplWidget.canvas.draw()
+        self.MplWidget2.canvas.axes.clear()
+        self.MplWidget2.canvas.draw()
+
+    def goto_borrar(self):
+        self.MplWidget.canvas.axes.clear()
+        self.MplWidget.canvas.draw()
+        self.MplWidget2.canvas.axes.clear()
+        self.MplWidget2.canvas.draw()
 
 cs = Curvespace()
 app = QApplication([])
--- frontend/mplwidget.py
+++ frontend/mplwidget.py
@@ -17,3 +17,17 @@ class MplWidget(QWidget):
 
         self.canvas.axes = self.canvas.figure.add_subplot(111)
         self.setLayout(vertical_layout)
+
+class MplWidget2(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        vertical_layout.addWidget(NavigationToolbar(self.canvas, self))
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
