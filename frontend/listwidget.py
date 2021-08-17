e5ae68cdb4be7917483ed6cffed8470628fa7db7
valen <vvenier@itba.edu.ar>
2021-08-17 02:30:08 -0300

    haciendo el menu de curvas 3

--- frontend/listwidget.py
+++ frontend/listwidget.py
@@ -1,21 +1,23 @@
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
+from backend import *
 
 class ListWidget(QWidget):
 
-    def __init__(self, nombre = None):
+    def __init__(self):
         QMainWindow.__init__(self)
         loadUi("ListWidget.ui", self)
 
-        self.nombre_list.setText(nombre)
-        #self.color.setStyleSheet("background-color:"+color)
+        self.nombre_list.setText(cs.curves[-1].name)
+        #self.color.setStyleSheet("background-color:" + color)
         self.visibilidad_list.clicked.connect(self.goto_visibilidad)
         self.color_list.clicked.connect(self.goto_color)
         self.datos_list.clicked.connect(self.goto_datos)
         self.borrar_list.clicked.connect(self.goto_borrar)
 
     def goto_visibilidad (self):
-        print("messi")
+        cs.curves[-1].change_visibility()
+
 
     def goto_color(self):
         print("messi")
@@ -25,5 +27,6 @@ class ListWidget(QWidget):
 
     def goto_borrar(self):
         print("messi")
+
 
 
